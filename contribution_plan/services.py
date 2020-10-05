5e0c0e54442b34552ca3baea62283d7da46c53b4
Damian Borowiecki <dborowiecki@soldevelo.com>
2020-10-05 13:09:49 +0200

    OFS-15: Added models and services shell for contribution plan

---
+++ contribution_plan/services.py
@@ -0,0 +1,41 @@
+import core
+from django.db import connection, transaction
+
+
+@core.comparable
+class ContributionPlanBundle(object):
+
+    def __init__(self, contribution_plan_bundle):
+        self.contribution_plan_bundle = contribution_plan_bundle
+        pass
+
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) and self.__dict__ == other.__dict__
+
+    def replace_propagation(self, insuree):
+        pass
+
+    def remove_propagation(self, insuree):
+        pass
+
+
+@core.comparable
+class ContributionPlanBundleDetails(object):
+
+    def __init__(self, contribution_plan_bundle_details):
+        self.contribution_plan_bundle_details = contribution_plan_bundle_details
+        pass
+
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) and self.__dict__ == other.__dict__
+
+
+@core.comparable
+class ContributionPlanBundle(object):
+
+    def __init__(self, contribution_plan):
+        self.contribution_plan = contribution_plan
+        pass
+
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) and self.__dict__ == other.__dict__
