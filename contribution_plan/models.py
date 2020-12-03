0a965f237e61b01e8a9409e4435284c211ef3a2b
sniedzielski <sniedzielski@soldevelo.com>
2020-12-03 12:40:01 +0100

    OFS-104: updated structures of models

--- contribution_plan/models.py
+++ contribution_plan/models.py
@@ -1,10 +1,7 @@
-import uuid
-
 from django.conf import settings
 from django.db import models
 from core import models as core_models, fields
 from graphql import ResolveInfo
-from jsonfallback.fields import FallbackJSONField
 from product.models import Product
 
 
@@ -17,28 +14,9 @@ class ContributionPlanBundleManager(models.Manager):
         return super(ContributionPlanBundleManager, self).filter(*args, **kwargs)
 
 
-class ContributionPlanBundle(core_models.UUIDVersionedModel):
-    id = models.AutoField(db_column='ContributionPlanBundleId', primary_key=True)
-    uuid = models.CharField(db_column='ContributionPlanBundleUUID', max_length=36, default=uuid.uuid4, unique=True)
-    version = models.IntegerField()
-
-    active = models.BooleanField(db_column='Active')
-    code = models.CharField(db_column='ContributionPlanBundleCode', max_length=255, blank=True, null=True)
-    name = models.CharField(db_column='ContributionPlanBundleName', max_length=255, blank=True, null=True)
-
-    json_ext = FallbackJSONField(db_column='Json_ext', blank=True, null=True)
-
-    date_created = fields.DateTimeField(db_column="DateCreated")
-    date_updated = fields.DateTimeField(db_column="DateUpdated", null=True)
-
-    user_updated = models.ForeignKey(core_models.User, db_column="UserUpdatedUUID",
-                                     related_name="%(class)s_UpdatedUUID", on_delete=models.deletion.DO_NOTHING,
-                                     null=True)
-    user_created = models.ForeignKey(core_models.User, db_column="UserCreatedUUID",
-                                     related_name="%(class)s_CreatedUUID", on_delete=models.deletion.DO_NOTHING)
-
-    date_valid_from = fields.DateTimeField(db_column="DateValidFrom")
-    date_valid_to = fields.DateTimeField(db_column="DateValidTo", null=True)
+class ContributionPlanBundle(core_models.HistoryBusinessModel):
+    code = models.CharField(db_column='Code', max_length=255, blank=True, null=True)
+    name = models.CharField(db_column='Name', max_length=255, blank=True, null=True)
 
     objects = ContributionPlanBundleManager()
 
@@ -66,31 +44,12 @@ class ContributionPlanManager(models.Manager):
         return super(ContributionPlanManager, self).filter(*args, **kwargs)
 
 
-class ContributionPlan(core_models.UUIDVersionedModel):
-    id = models.AutoField(db_column='ContributionPlanId', primary_key=True)
-    uuid = models.CharField(db_column='ContributionPlanUUID', max_length=36, default=uuid.uuid4, unique=True)
-    version = models.IntegerField()
-
-    active = models.BooleanField(db_column='Active')
-
-    code = models.CharField(db_column='ContributionPlanCode', max_length=255, blank=True, null=True)
+class ContributionPlan(core_models.HistoryBusinessModel):
+    code = models.CharField(db_column="Code", max_length=255, blank=True, null=True)
+    name = models.CharField(db_column="Name", max_length=255, blank=True, null=True)
     # calculation = models.ForeignKey(Calculation, db_column="CalculationUUID", on_delete=models.deletion.DO_NOTHING)
     benefit_plan = models.ForeignKey(Product, db_column="BenefitPlanUUID", on_delete=models.deletion.DO_NOTHING)
-    periodicity = models.IntegerField()
-
-    json_ext = FallbackJSONField(db_column='Json_ext', blank=True, null=True)
-
-    date_created = fields.DateTimeField(db_column="DateCreated")
-    amendment = models.IntegerField()
-    date_updated = fields.DateTimeField(db_column="DateUpdated", null=True)
-
-    user_updated = models.ForeignKey(core_models.User, db_column="UserUpdatedUUID", null=True,
-                                     related_name="%(class)s_UpdatedUUID", on_delete=models.deletion.DO_NOTHING)
-    user_created = models.ForeignKey(core_models.User, db_column="UserCreatedUUID",
-                                     related_name="%(class)s_CreatedUUID", on_delete=models.deletion.DO_NOTHING)
-
-    date_valid_from = fields.DateTimeField(db_column="DateValidFrom")
-    date_valid_to = fields.DateTimeField(db_column="DateValidTo", null=True)
+    periodicity = models.IntegerField(db_column="Periodicity", blank=True, null=True)
 
     objects = ContributionPlanManager()
 
@@ -118,32 +77,11 @@ class ContributionPlanBundleDetailsManager(models.Manager):
         return super(ContributionPlanBundleDetailsManager, self).filter(*args, **kwargs)
 
 
-class ContributionPlanBundleDetails(core_models.UUIDVersionedModel):
-    id = models.AutoField(db_column='ContributionPlanBundleDetailsId', primary_key=True)
-    uuid = models.CharField(db_column='ContributionPlanBundleDetailsUUID', max_length=36,
-                            default=uuid.uuid4, unique=True)
-    version = models.IntegerField()
-
+class ContributionPlanBundleDetails(core_models.HistoryBusinessModel):
     contribution_plan_bundle = models.ForeignKey(ContributionPlanBundle, db_column="ContributionPlanBundleUUID",
                                                  on_delete=models.deletion.DO_NOTHING)
-
     contribution_plan = models.ForeignKey(ContributionPlan, db_column="ContributionPlanUUID",
                                           on_delete=models.deletion.DO_NOTHING)
-
-    json_ext = FallbackJSONField(db_column='Json_ext', blank=True, null=True)
-
-    date_created = fields.DateTimeField(db_column="DateCreated")
-    date_updated = fields.DateTimeField(db_column="DateUpdated", null=True)
-
-    user_updated = models.ForeignKey(core_models.User, db_column="UserUpdatedUUID", null=True,
-                                     related_name="%(class)s_UpdatedUUID", on_delete=models.deletion.DO_NOTHING)
-    user_created = models.ForeignKey(core_models.User, db_column="UserCreatedUUID",
-                                     related_name="%(class)s_CreatedUUID", on_delete=models.deletion.DO_NOTHING)
-
-    date_valid_from = fields.DateTimeField(db_column="DateValidFrom")
-    date_valid_to = fields.DateTimeField(db_column="DateValidTo", null=True)
-
-    active = models.BooleanField(db_column='Active')
 
     objects = ContributionPlanBundleDetailsManager()
 
