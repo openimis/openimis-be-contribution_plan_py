96e6ebe65fb7dec9736b34b18dff7d0d68d5f7ef
sniedzielski <sniedzielski@soldevelo.com>
2020-12-17 13:03:05 +0100

    OFS-60: services for models in CP and CPB module

--- contribution_plan/services.py
+++ contribution_plan/services.py
@@ -1,11 +1,7 @@
-import core
 import json
 
 from django.core.serializers.json import DjangoJSONEncoder
-from django.core.exceptions import PermissionDenied
-from django.db import connection, transaction
 from django.contrib.auth.models import AnonymousUser
-from django.core import serializers
 from django.forms.models import model_to_dict
 from contribution_plan.models import ContributionPlan as ContributionPlanModel, ContributionPlanBundle as ContributionPlanBundleModel, \
     ContributionPlanBundleDetails as ContributionPlanBundleDetailsModel
@@ -25,6 +21,79 @@ def check_authentication(function):
     return wrapper
 
 
+class ContributionPlan(object):
+
+    def __init__(self, user):
+        self.user = user
+
+    @check_authentication
+    def get_by_id(self, by_contribution_plan):
+        try:
+            cp = ContributionPlanModel.objects.get(id=by_contribution_plan.id)
+            uuid_string = str(cp.id)
+            dict_representation = model_to_dict(cp)
+            dict_representation["id"], dict_representation["uuid"] = (str(uuid_string), str(uuid_string))
+        except Exception as exc:
+            return _output_exception(model_name="ContributionPlan", method="get", exception=exc)
+        return _output_result_success(dict_representation=dict_representation)
+
+    @check_authentication
+    def create(self, contribution_plan):
+        try:
+            cp = ContributionPlanModel(**contribution_plan)
+            cp.save(username=self.user.username)
+            uuid_string = str(cp.id)
+            dict_representation = model_to_dict(cp)
+            dict_representation["id"], dict_representation["uuid"] = (str(uuid_string), str(uuid_string))
+        except Exception as exc:
+            return _output_exception(model_name="ContributionPlan", method="create", exception=exc)
+        return _output_result_success(dict_representation=dict_representation)
+
+    @check_authentication
+    def update(self, contribution_plan):
+        try:
+            updated_cp = ContributionPlanModel.objects.filter(id=contribution_plan['id']).first()
+            [setattr(updated_cp, key, contribution_plan[key]) for key in contribution_plan]
+            updated_cp.save(username=self.user.username)
+            uuid_string = str(updated_cp.id)
+            dict_representation = model_to_dict(updated_cp)
+            dict_representation["id"], dict_representation["uuid"] = (str(uuid_string), str(uuid_string))
+        except Exception as exc:
+            return _output_exception(model_name="ContributionPlan", method="update", exception=exc)
+        return _output_result_success(dict_representation=dict_representation)
+
+    @check_authentication
+    def delete(self, contribution_plan):
+        try:
+            cp_to_delete = ContributionPlanModel.objects.filter(id=contribution_plan['id']).first()
+            cp_to_delete.delete(username=self.user.username)
+            return {
+                "success": True,
+                "message": "Ok",
+                "detail": "",
+            }
+        except Exception as exc:
+            return _output_exception(model_name="ContributionPlan", method="delete", exception=exc)
+
+    @check_authentication
+    def replace(self, contribution_plan):
+        try:
+            cp_to_replace = ContributionPlanModel.objects.filter(id=contribution_plan['uuid']).first()
+            cp_to_replace.replace_object(data=contribution_plan, username=self.user.username)
+            uuid_string = str(cp_to_replace.id)
+            dict_representation = model_to_dict(cp_to_replace)
+            dict_representation["id"], dict_representation["uuid"] = (str(uuid_string), str(uuid_string))
+        except Exception as exc:
+            return _output_exception(model_name="ContributionPlan", method="replace", exception=exc)
+        return {
+            "success": True,
+            "message": "Ok",
+            "detail": "",
+            "old_object": json.loads(json.dumps(dict_representation, cls=DjangoJSONEncoder)),
+            "uuid_new_object": str(cp_to_replace.replacement_uuid),
+        }
+
+
 class ContributionPlanBundle(object):
 
     def __init__(self, user):
@@ -33,9 +102,9 @@ class ContributionPlanBundle(object):
     @check_authentication
     def get_by_id(self, by_contribution_plan_bundle):
         try:
-            ph = ContributionPlanBundleModel.objects.get(id=by_contribution_plan_bundle.id)
-            uuid_string = str(ph.id)
-            dict_representation = model_to_dict(ph)
+            cpb = ContributionPlanBundleModel.objects.get(id=by_contribution_plan_bundle.id)
+            uuid_string = str(cpb.id)
+            dict_representation = model_to_dict(cpb)
             dict_representation["id"], dict_representation["uuid"] = (str(uuid_string), str(uuid_string))
         except Exception as exc:
             return _output_exception(model_name="ContributionPlanBundle", method="get", exception=exc)
@@ -82,7 +151,7 @@ class ContributionPlanBundle(object):
     @check_authentication
     def replace(self, contribution_plan_bundle):
         try:
-            cpb_to_replace = ContributionPlanBundleModel.objects.filter(id=contribution_plan_bundle['uuid']).first()
+            cpb_to_replace = ContributionPlanModel.objects.filter(id=contribution_plan_bundle['uuid']).first()
             cpb_to_replace.replace_object(data=contribution_plan_bundle, username=self.user.username)
             uuid_string = str(cpb_to_replace.id)
             dict_representation = model_to_dict(cpb_to_replace)
@@ -97,38 +166,61 @@ class ContributionPlanBundle(object):
             "uuid_new_object": str(cpb_to_replace.replacement_uuid),
         }
 
-@core.comparable
+
 class ContributionPlanBundleDetails(object):
 
-    def __init__(self, contribution_plan_bundle_details):
-        self.contribution_plan_bundle_details = contribution_plan_bundle_details
-        pass
+    def __init__(self, user):
+        self.user = user
 
-    def __eq__(self, other):
-        return isinstance(other, self.__class__) and self.__dict__ == other.__dict__
+    @check_authentication
+    def get_by_id(self, by_contribution_plan_bundle_details):
+        try:
+            cpbd = ContributionPlanBundleDetailsModel.objects.get(id=by_contribution_plan_bundle_details.id)
+            uuid_string = str(cpbd.id)
+            dict_representation = model_to_dict(cpbd)
+            dict_representation["id"], dict_representation["uuid"] = (str(uuid_string), str(uuid_string))
+        except Exception as exc:
+            return _output_exception(model_name="ContributionPlanBundleDetails", method="get", exception=exc)
+        return _output_result_success(dict_representation=dict_representation)
 
+    @check_authentication
+    def create(self, contribution_plan_bundle_details):
+        try:
+            cpbd = ContributionPlanBundleDetailsModel(**contribution_plan_bundle_details)
+            cpbd.save(username=self.user.username)
+            uuid_string = str(cpbd.id)
+            dict_representation = model_to_dict(cpbd)
+            dict_representation["id"], dict_representation["uuid"] = (str(uuid_string), str(uuid_string))
+        except Exception as exc:
+            return _output_exception(model_name="ContributionPlanBundleDetails", method="create", exception=exc)
+        return _output_result_success(dict_representation=dict_representation)
 
-@core.comparable
-class ContributionPlan(object):
+    @check_authentication
+    def update(self, contribution_plan_bundle_details):
+        try:
+            updated_cpbd = ContributionPlanBundleDetailsModel.objects.filter(
+                id=contribution_plan_bundle_details['id']).first()
+            [setattr(updated_cpbd, key, contribution_plan_bundle_details[key]) for key in contribution_plan_bundle_details]
+            updated_cpbd.save(username=self.user.username)
+            uuid_string = str(updated_cpbd.id)
+            dict_representation = model_to_dict(updated_cpbd)
+            dict_representation["id"], dict_representation["uuid"] = (str(uuid_string), str(uuid_string))
+        except Exception as exc:
+            return _output_exception(model_name="ContributionPlanBundleDetails", method="update", exception=exc)
+        return _output_result_success(dict_representation=dict_representation)
 
-    def __init__(self, contribution_plan):
-        self.contribution_plan = contribution_plan
-        pass
-
-    def __eq__(self, other):
-        return isinstance(other, self.__class__) and self.__dict__ == other.__dict__
-
-    def replace_propagation(self, contribution_plan_bundle):
-        pass
-
-    def remove_propagation(self, contribution_plan_bundle):
-        pass
-
-    def create(self, contribution_plan):
-        pass
-
-    def update(self, contribution_plan):
-        pass
+    @check_authentication
+    def delete(self, contribution_plan_bundle_details):
+        try:
+            cpbd_to_delete = ContributionPlanBundleDetailsModel.objects.filter(id=contribution_plan_bundle_details['id']).first()
+            cpbd_to_delete.delete(username=self.user.username)
+            return {
+                "success": True,
+                "message": "Ok",
+                "detail": "",
+            }
+        except Exception as exc:
+            return _output_exception(model_name="ContributionPlanBundleDetails", method="delete", exception=exc)
 
 
 def _output_exception(model_name, method, exception):
