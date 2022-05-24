bdf6bb0e02226ff741366a88dec53d69664b5a98
sniedzielski <sniedzielski@soldevelo.com>
2022-05-24 09:37:39 +0200

    OTC-594: moved obtain calcrule params into ContributionPlan

---
+++ contribution_plan/utils.py
@@ -0,0 +1,30 @@
+import json
+
+from contribution_plan.models import GenericPlan
+
+
+def obtain_calcrule_params(payment_plan: GenericPlan,
+    integer_param_list: list, none_integer_param_list: list) -> dict:
+    # obtaining payment plan params saved in payment plan json_ext fields
+    pp_params = payment_plan.json_ext
+    if isinstance(pp_params, str):
+        pp_params = json.loads(pp_params)
+    if pp_params:
+        pp_params = pp_params["calculation_rule"] if "calculation_rule" in pp_params else None
+    # correct empty string values
+
+    for key in integer_param_list:
+        if key in pp_params.items():
+            value = pp_params.items()[f'{key}']
+            if value == "":
+                pp_params[f'{key}'] = 0
+            else:
+                pp_params[f'{key}'] = int(value)
+        else:
+            pp_params[f'{key}'] = 0
+
+    for key in none_integer_param_list:
+        if key not in pp_params.items():
+            pp_params[f'{key}'] = None
+
+    return pp_params
