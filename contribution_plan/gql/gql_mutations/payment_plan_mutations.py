aca21612bb0a8b7bde51579252c2df6f10b461e2
Jan <j.dolkowski@soldevelo.com>
2024-03-18 14:09:04 +0100

    hotfix: vailidate payment plan code

--- contribution_plan/gql/gql_mutations/payment_plan_mutations.py
+++ contribution_plan/gql/gql_mutations/payment_plan_mutations.py
@@ -50,7 +50,7 @@ class UpdatePaymentPlanMutation(BaseHistoryModelUpdateMutationMixin, BaseMutatio
                 ContributionPlanConfig.gql_mutation_update_paymentplan_perms):
             raise ValidationError(_("mutation.authentication_required"))
 
-        if PaymentPlanService.check_unique_code(data['code']):
+        if PaymentPlanService.check_unique_code(data['code'], data['id']):
             raise ValidationError(_("mutation.payment_plan_code_duplicated"))
 
 
